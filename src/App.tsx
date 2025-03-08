9c781985da26f7a826143e128796a6a42110978f
farrel irawan <irawanssfarrel@gmail.com>
2025-03-09 03:32:54 +0700

    membuat kerangka komponen Profile Body

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
     <>
       <ContentWrapper>
         <ProfileHeader />
+        <section>
+          <div className="container w-full">
+            <div className="">
+              <h2 className="text-3xl leading-relaxed font-semibold text-blue-500">Farrel Irawan</h2>
+              <p className="text-gray-600">Frontend Developer</p>
+            </div>
+          </div>
+        </section>
       </ContentWrapper>
     </>
   )
